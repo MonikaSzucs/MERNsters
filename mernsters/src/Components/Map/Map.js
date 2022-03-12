a5852694a32a22e08a36e0a64e0b29e6312e9507
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-03-12 14:11:18 -0800

    Catched all Fire Events JSON
    was able to console log all the events

---
+++ mernsters/src/Components/Map/Map.js
@@ -0,0 +1,11 @@
+import GoogleMapReact from 'google-map-react';
+
+const Map = ({ center, zoom }) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Map
--- mernsters/src/Pages/Home.js
+++ mernsters/src/Pages/Home.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css';
 import { Container, Row, Col } from 'react-grid-system';
 import NavBar from '../Components/NavBar';
+import axios from 'axios';
+
 
 // Grid System: npm install react-grid-system --save
 export default function Home() {
+
+    const apiKey = 'LXpdsX3u0yZ2PTSR42dTLzeojjgOtlOYw3rTymp6'
+    const [eventData, setEventData] = useState([])
+    const [loading, setLoading] = useState(false)
+    const url = 'https://eonet.gsfc.nasa.gov/api/v2.1/categories';
+
+    const fetchEvents = async () => {
+        setLoading(true)
+        const res = await fetch('https://eonet.gsfc.nasa.gov/api/v2.1/events')
+        const { events } = await res.json()
+        console.log("test")
+        console.log(events)
+        setEventData(events)
+        setLoading(false)
+    }
+
+    useEffect(() => {
+        console.log("outside")
+        fetchEvents()
+    })
+
     return (
         <div>
             <NavBar/>
@@ -15,6 +38,9 @@ export default function Home() {
                 </Col>
                 <Col sm={4}>
                 Home page
+                    <div>
+                    
+                    </div>
                 </Col>
                 <Col sm={4}>
                 Home page
