fc957687d3402a4d8d99541cfec9a553938c1a80
GoodestUsername <ericdongcannn@hotmail.com>
2022-03-12 22:31:35 -0800

    fetcn data

--- mernsters/src/Components/Card/Card.js
+++ mernsters/src/Components/Card/Card.js
@@ -2,28 +2,40 @@ import React from 'react';
 
 const Card = (props) => {
     const allData = props.allData
-    console.log("CARD HERE")
-    let cardItems;
-    if(typeof allData !== 'undefined' && allData !=null){
-        console.log("not undefined")
-        console.log(allData)
-        console.log(allData.id)
-        console.log(allData.title)
-        console.log(allData.link)
 
-        cardItems = 
-            <div>
-                <div>{allData.id}</div>
-                <div>{allData.title}</div>
-                <div></div>{allData.link}
-                {props.namingType}
-            </div>
-    }
-    if (!props){
-        return null;
-    }
+    // console.log("CARD HERE")
+    // let cardItems;
+    // if(typeof allData !== 'undefined' && allData !=null){
+        // console.log("not undefined")
+        // console.log(allData)
+        // console.log(allData.id)
+        // console.log(allData.title)
+        // console.log(allData.link)
+
+    //     cardItems = 
+    //         <div>
+    //             <div>{allData.id}</div>
+    //             <div>{allData.title}</div>
+    //             <div></div>{allData.link}
+    //             {props.namingType}
+    //         </div>
+    // }
+    // if (!props){
+    //     return null;
+    // }
     
-    return cardItems;
+    // return cardItems;
+    return (
+            <div>
+                 <div>{allData.id}</div>
+                 <div>{allData.title}</div>
+                 <div></div>{allData.link}
+                 <br></br>
+                 <br></br>
+                 <br></br>
+                 <br></br>
+            </div>
+    )
     
 }
 
--- mernsters/src/Pages/Home.js
+++ mernsters/src/Pages/Home.js
@@ -14,46 +14,69 @@ export default function Home() {
     const [counter, setCounter] = useState(0);
     const [dataPassed, setDataPassed] = useState()
 
-
     const increase = () => {
         if (counter == 0) {
           setCounter(count => count + 1);
         }
       };
 
+    function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lon2-lon1); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km
+    return d;
+    }
     
+    function deg2rad(deg) {
+    return deg * (Math.PI/180)
+    }
 
     const fetchEvent = () => axios.get('https://eonet.gsfc.nasa.gov/api/v2.1/events').then((response) => {
         const lastGeometries = response.data.events[0].geometries.length - 1
-        const data = response.data.events.slice(0,20)
-        console.log(data)
-        //let clusterGrabbed = []
-        
-        // let gary = {"id": response.data.events[0].id, 
-        //             "title": response.data.events[0].title,
-        //             "link": response.data.events[0].link
+        // const data = response.data.events.slice(0,20)
+        // const data = response.data.events.filter(e => {
+        //     if (getDistanceFromLatLonInKm(56.1304, 106.3468, e.geometries[0].coordinates[0], e.geometries[-1].coordinates[1]
+        //         ) < 300) {
+        //             return e;
         //         }
-        // var clusterCreated = clusterGrabbed.push(gary)
-        // console.log(clusterGrabbed)
+        // })
+//56.1304° N, 106.3468° W
+        console.log(data)
+        setEventData(data)
+        let clusterGrabbed = []
+        
+        let gary = {"id": response.data.events[0].id, 
+                    "title": response.data.events[0].title,
+                    "link": response.data.events[0].link
+                }
+        var clusterCreated = clusterGrabbed.push(gary)
+        console.log(clusterGrabbed)
 
-        // setDataPassed(gary)
+        setDataPassed(gary)
         
     }).catch((err)=>{
         console.log(err)
     },[])
 
-    // useEffect(() => {
-    //     console.log(counter)
-    //     console.log("outside")
-    //     if(counter === 0){
-    //       fetchEvent(); increase()
-    //       console.log("one")
-    //     }
-    //     if(counter !== 0){
-    //       setInterval(fetchEvent, 100000);
-    //       console.log("two")
-    //     }
-    // })
+    useEffect(() => {
+        console.log(counter)
+        console.log("outside")
+        if(counter === 0){
+          fetchEvent(); increase()
+          console.log("one")
+        }
+        if(counter !== 0){
+          setInterval(fetchEvent, 100000);
+          console.log("two")
+        }
+    })
 
     return (
         <div>
@@ -66,12 +89,12 @@ export default function Home() {
                 <Col sm={4}>
                 Home page
                     <div>
-                        {/* {data.map((eventsHappening) => 
+                        {eventData.map((eventsHappening) => 
                             <Card 
-                            allData={dataPassed}
+                            allData={eventsHappening}
                             />
                         )}
-                         */}
+                        
                     </div>
                 </Col>
                 <Col sm={4}>
