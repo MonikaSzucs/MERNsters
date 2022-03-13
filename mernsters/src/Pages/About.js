9c201a3be6b65aa91a75c2b708934f1978f3da47
Stefan Chen <zchen158@my.bcit.ca>
2022-03-13 01:55:37 -0800

    Add content to about page

--- mernsters/src/Pages/About.js
+++ mernsters/src/Pages/About.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-grid-system';
-import NavBar from '../Components/NavBar/navbar';
+import React from "react";
+import { Container, Row, Col } from "react-grid-system";
+import NavBar from "../Components/NavBar/navbar";
 
 export default function About() {
-    return (
-        <div>
-            <NavBar/>
-            <Container>
-            <Row>
-                <Col sm={4}>
-                
-                </Col>
-                <Col sm={4}>
-                About HERE
-                </Col>
-                <Col sm={4}>
-                
-                </Col>
-            </Row>
-            </Container>
-        </div>
-    )
+  return (
+    <div>
+      <div>
+        <NavBar />
+        <Container>
+          <Row>
+            <Col sm={4}></Col>
+            <Col sm={4}>About HERE</Col>
+            <Col sm={4}></Col>
+          </Row>
+        </Container>
+      </div>
+      <p>
+        Mernsters Firewatch is a real time surveillance app that can show the
+        locations of wildfires. It enables users to see exactly where the
+        wildfire is happending so they can better preprare. In the future, we
+        might implement a warning function to give user an alert if the user is
+        whithin a certain distance of an active wildfire.
+      </p>
+      <h3>
+        Members(Mernsters) in our group: Monika Szucs, Eric Dong and
+        Stefan Chen
+      </h3>
+
+        API used in this project:
+        <li> NASA Earth Observatory Natural Event Tracker <br></br>https://eonet.gsfc.nasa.gov/api/v2.1/events</li>
+      
+    </div>
+  );
 }
 // About
--- mernsters/src/Pages/Login.js
+++ mernsters/src/Pages/Login.js
@@ -1,37 +1,32 @@
-import React, {useState} from 'react';
-import { Container, Row, Col } from 'react-grid-system';
-import NavBar from '../Components/NavBar/navbar';
+import React, { useState } from "react";
+import { Container, Row, Col } from "react-grid-system";
+import NavBar from "../Components/NavBar/navbar";
 import Form from "react-bootstrap/Form";
-import Button from 'react-bootstrap/Button';
-import './Login.css'
-
+import Button from "react-bootstrap/Button";
+import "./Login.css";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    function validateForm() {
-        return email.length > 0 && password.length > 0;
-    }
-    
-    function handleSubmit(event) {
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
     event.preventDefault();
-    }
-    
-    return (
-      <div>  
-        <NavBar />
-          
-        <div className="Login">
+  }
+
+  return (
+    <div>
+      <NavBar />
+
+      <div className="Login">
         <Container>
           <Row>
-            <Col sm={4}>
-            </Col>
-            <Col sm={4}>
-              Login HERE
-            </Col>
-            <Col sm={4}>
-            </Col>
+            <Col sm={4}></Col>
+            <Col sm={4}>Login HERE</Col>
+            <Col sm={4}></Col>
           </Row>
         </Container>
 
@@ -42,21 +37,23 @@ export default function Login() {
               autoFocus
               type="email"
               value={email}
-              onChange={(e) => setEmail(e.target.value)} />
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </Form.Group>
           <Form.Group size="lg" controlId="password">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)} />
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Form.Group>
           <Button block size="lg" type="submit" disabled={!validateForm()}>
             Log in
           </Button>
         </Form>
-        </div>
       </div>
-    )
+    </div>
+  );
 }
 // Login
